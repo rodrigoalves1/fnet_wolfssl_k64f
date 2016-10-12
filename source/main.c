190b2a2530024b0dcb490badf8f8de0bff49a0c3
Rodrigo Alves <raan@cin.ufpe.br>
2016-10-12 17:51:20 -0300

    Update main

--- source/main.c
+++ source/main.c
@@ -4,10 +4,32 @@
  *  Created on: 10/05/2016
  *      Author: RodrigoA
  */
+#ifndef BENCHMARK
+#include "board.h"
+#include "pin_mux.h"
+#include "clock_config.h"
 #include "fapp.h" /* FNET API */
+#include "fsl_debug_console.h"
+#include "fsl_device_registers.h"
+
+
 /* main entry point*/
 int main(void)
 {
+    /* Init hardware*/
+    BOARD_InitPins();
+    BOARD_BootClockRUN();
+    BOARD_InitDebugConsole();
+
+    /* Init time measurement. SysTick method deployed.
+        if (time_config())
+        {
+            PRINTF("ERROR in SysTick Configuration\r\n");
+        }
+        else
+        {
+        	fnet_printf("Clock ok.\n");
+        }*/
 
     /* Init UART */
     fnet_cpu_serial_init(FNET_CFG_CPU_SERIAL_PORT_DEFAULT, 115200u);
@@ -34,3 +56,4 @@ int main(void)
     {
     }*/
 }
+#endif
